a7ef9636a828987bcc2d12db92ed6c7b1a147417
_ignacio.dev_ <protec83007@gmail.com>
2022-04-24 14:56:13 -0300

    Prefix

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { Client, Intents } = require('discord.js');
 require('dotenv').config()
 
+const prefix = process.env.PREFIX;
+
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
@@ -9,6 +11,16 @@ const client = new Client({
 
 client.once("ready", (bot) => {
     console.log(`Bot: ${bot.user.username}\nStatus: ${bot.presence.status}`);
+});
+
+client.on("messageCreate",(msg)=>{
+    if (msg.author.bot)
+        return console.log(`Mensaje del Bot: ${msg.author.username}`);
+    
+    if (msg.content.startsWith(prefix)) 
+        return msg.reply(`Este es un comando del prefijo ${prefix}`);
+
+    msg.reply("Esto no es un comando");
 });
 
 // Token
